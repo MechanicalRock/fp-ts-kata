a35d8be55ce3dcafee5d51a66377d72df23afca2
zainab <maleki68.net@gmail.com>
2021-02-10 17:15:21 +0800

    chain but not completed yet

---
+++ Z/chain/chain.ts
@@ -0,0 +1,21 @@
+import { pipe } from 'fp-ts/lib/function';
+import { bind, bindTo, IOEither, right } from 'fp-ts/IOEither';
+
+export const add = (a: number): IOEither<Error, number> => {
+  if (a < 10) {
+    return right(a + 10);
+  } else throw new Error('your number must be less than 10');
+};
+
+const textDetails = (firstnumber: number, result: number): IOEither<Error, string> => {
+  return right(`${firstnumber} + 10 equals ${result} `);
+};
+
+const result = () =>
+  pipe(
+    add(4),
+    bind('textDetails', (addedNumber) => textDetails(4, addedNumber.result)),
+    (finalObject) => console.log(JSON.stringify(finalObject)),
+  );
+
+result();
