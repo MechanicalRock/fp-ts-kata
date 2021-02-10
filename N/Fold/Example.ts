1bb46bbf1641b9c51742491196a113478370b5d1
reyhani.ronak@gmail.com <reyhani.ronak@gmail.com>
2021-02-10 15:17:33 +0800

    Fold

---
+++ N/Fold/Example.ts
@@ -0,0 +1,29 @@
+import { fold, left, right } from "fp-ts/Either";
+import { add, multiply } from "../Pipe/Example";
+
+//export declare function fold<E, A, B>(onLeft: (e: E) => B, onRight: (a: A) => B): (ma: Either<E, A>) => B
+
+const operation = fold<Error, number, unknown>((e: Error) => e, add);
+
+const usernameLength = fold(
+  () => 0,
+  (username: string) => username.length,
+);
+
+const usernameOne = "user-name";
+const usernameTwo = "";
+
+const rightResult: number = usernameLength(right(usernameOne));
+const leftResult = usernameLength(right(usernameTwo));
+
+const rightoperation = operation(right(3));
+const leftoperation = operation(left(Error("Error"))); // would expect to throw and error
+
+const results = () => {
+  console.log("string length is", rightResult);
+  console.log("empty string length is", leftResult);
+  console.log("Add operation when proper value is passed to function", rightoperation);
+  console.log("Add operation with not accepted input", leftoperation);
+};
+
+results();
