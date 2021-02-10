0bb604fbad989739c673c055ff8edb5c3f2660c6
zainab <maleki68.net@gmail.com>
2021-02-10 17:07:32 +0800

    fixed the issue with chain first and added a map and fold

--- Z/chainFirst/chainFirst.ts
+++ Z/chainFirst/chainFirst.ts
@@ -1,22 +1,26 @@
 import { pipe } from 'fp-ts/lib/function';
-import { chainFirst, fromIO, IOEither, map, right } from 'fp-ts/IOEither';
+import { chainFirst, fromIO, IOEither, map, right, left, fold } from 'fp-ts/IOEither';
+
+import { of } from 'fp-ts/IO';
 
 export const add = (a: number): IOEither<Error, number> => {
   if (a < 10) {
     return right(a + 10);
-  } else new Error('your number must be less than 10');
+  } else return left(new Error('your number must be less than 10'));
 };
 
 const secondAdd = (result: number): number => {
   return result + 20;
 };
 
-const checkItem = chainFirst((result) => fromIO(() => console.log(result)));
-
 const result = pipe(
-  add(12),
+  add(4),
   chainFirst((result) => fromIO(() => console.log(result))),
   map(secondAdd),
+  fold(
+    () => of(100),
+    (a) => of(a + 200),
+  ),
 );
 
 console.log(result());
