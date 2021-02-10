bd840bc87c6392805f222190714838b255021680
Josh Crane <josh.crane@mechanicalrock.io>
2021-02-10 13:00:17 +0800

    new task stuff

---
+++ J/task.ts
@@ -0,0 +1,13 @@
+import * as fp from "fp-ts";
+import { Task } from "fp-ts/Task"
+
+// interface Task<A> {
+// (): Promise<A>
+//}
+
+// A task<string> is a function that returns the promise of a string for example
+// Tasks are ALWAYS expected to succeed 
+
+const ATask: Task<number> = ():Promise<number> => new Promise(() => 1); 
+
+// Theres nothing really special about it!
